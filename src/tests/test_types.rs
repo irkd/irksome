32eee9699281084ba3a0f70db78b4717b4413315
Barret Rennie <barret@brennie.ca>
2018-01-31 01:40:17 -0600

    Add types::MessageKind

--- src/tests/test_types.rs
+++ src/tests/test_types.rs
@@ -1,4 +1,4 @@
-use types::Source;
+use types::*;
 
 #[test]
 fn test_display_source() {
@@ -32,5 +32,38 @@ fn test_display_source() {
             }
         ),
         "foo!~bar@unaffilliated/users/foo"
+    );
+}
+
+#[test]
+fn test_display_message_kind() {
+    assert_eq!(format!("{}", MessageKind::Numeric(0)), "000");
+    assert_eq!(format!("{}", MessageKind::Numeric(1)), "001");
+    assert_eq!(format!("{}", MessageKind::Numeric(12)), "012");
+    assert_eq!(format!("{}", MessageKind::Numeric(123)), "123");
+    assert_eq!(
+        format!("{}", MessageKind::Command("privmsg".into())),
+        "PRIVMSG"
+    );
+    assert_eq!(
+        format!("{}", MessageKind::Command("PRIVMSG".into())),
+        "PRIVMSG"
+    );
+}
+
+#[test]
+fn test_eq_message_kind() {
+    assert_eq!(MessageKind::Numeric(0), MessageKind::Numeric(0));
+    assert_eq!(
+        MessageKind::Command("privmsg".into()),
+        MessageKind::Command("privmsg".into())
+    );
+    assert_eq!(
+        MessageKind::Command("PRIVMSG".into()),
+        MessageKind::Command("privmsg".into())
+    );
+    assert_ne!(
+        MessageKind::Command("privmsg".into()),
+        MessageKind::Command("notice".into())
     );
 }
--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,7 @@
 use std::fmt;
 
-/// A message source.
 #[derive(Debug, Eq, PartialEq)]
+/// A message source.
 pub enum Source {
     /// A server message source.
     Server {
@@ -28,6 +28,38 @@ impl fmt::Display for Source {
                 ref ident,
                 ref host,
             } => write!(f, "{}!{}@{}", nick, ident, host),
+        }
+    }
+}
+
+#[derive(Debug, Eq)]
+/// The kind of a message.
+pub enum MessageKind {
+    /// A command.
+    ///
+    /// Commands are case-insensitive.
+    Command(String),
+    /// A numeric reply to a command, consisting of a 3-digit number.
+    Numeric(u16),
+}
+
+impl fmt::Display for MessageKind {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            MessageKind::Command(ref c) => write!(f, "{}", c.to_ascii_uppercase()),
+            MessageKind::Numeric(n) => write!(f, "{:03}", n),
+        }
+    }
+}
+
+impl PartialEq for MessageKind {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (&MessageKind::Command(ref l), &MessageKind::Command(ref r)) => {
+                l.eq_ignore_ascii_case(r)
+            }
+            (&MessageKind::Numeric(ref l), &MessageKind::Numeric(ref r)) => l == r,
+            _ => false,
         }
     }
 }
